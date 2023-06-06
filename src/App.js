86f82234273c03f18354aac8965bafe858e84395
geniuska <aigojie2020@gmail.com>
2023-06-06 10:55:01 +0100

    realtor app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signout from "./pages/Signout";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/Sign-in" element={<Signin />} />
+        <Route path="/Sign-out" element={<Signout />} />
+        <Route path="/Forgot-Password" element={<ForgotPassword />} />
+        <Route path="/Offers" element={<Offers />} />
+      </Routes>
+    </Router>
   );
 }
 
